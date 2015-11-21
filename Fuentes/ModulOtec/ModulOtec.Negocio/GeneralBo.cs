7b29d426dc69d75437c97c76b013086049d4a557
Omar Carmona Rivas <ocarmona07@gmail.com>
2015-11-21 19:37:22 -0300

    Se crea estructura base del proyecto

---
+++ Fuentes/ModulOtec/ModulOtec.Negocio/GeneralBo.cs
@@ -0,0 +1,11 @@
+﻿namespace ModulOtec.Negocio
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class GeneralBo
+    {
+    }
+}
