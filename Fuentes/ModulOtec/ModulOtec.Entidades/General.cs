109f2fa91d4dfd1683327864d918594466e9e4e6
Omar Carmona Rivas <ocarmona07@gmail.com>
2015-11-25 22:29:08 -0300

    Se agrega capa de entidades

---
+++ Fuentes/ModulOtec/ModulOtec.Entidades/General.cs
@@ -0,0 +1,11 @@
+﻿namespace ModulOtec.Entidades
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class General
+    {
+    }
+}
