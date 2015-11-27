20efdedd32b8ec2884dca8ad23e4375a9e6d1f0c
oibz <curarrehueinfo@gmail.com>
2015-11-26 22:06:13 -0300

    Se completa en primera instancia la vista de Análisis de no conformidad, se agrega el detalle de los datos de no conformidad, que posteriormente en la capa de negocios serán asociados con el ingreso de no conformidades

--- Fuentes/ModulOtec/ModulOtec.Vista/AnalizarNoConformidad.aspx
+++ Fuentes/ModulOtec/ModulOtec.Vista/AnalizarNoConformidad.aspx
@@ -2,7 +2,66 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    <h3>Analisis de No Conformidad</h3>
+    <h4>Analisis de No Conformidad</h4>
     <hr />
-    <p>ID:</p><span></span>
+    <div class="panel panel-primary">
+        <div class="panel-heading">Información de la No Conformidad</div>
+        <div class="panel-body">
+                <!-- Área de desglose de la no conformidad ingresada desde "IngresarNoConformidad" -->
+                <p><strong>Código Servicio No Conforme:</strong>
+                        <asp:Label ID="lblCodSNC" runat="server" Text="SNC0000"></asp:Label>
+                </p>
+            <div class="row">
+                <div class="col-md-4">
+                    <p>
+                        <strong>Fecha de Ingreso:</strong>
+                        <asp:Label ID="lblFechaIngreso" runat="server" Text="00/00/0000"></asp:Label>
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <p><strong>Fecha Identificación:</strong>
+                            <asp:Label ID="lblFechaIdentificacion" runat="server" Text="00/00/0000"></asp:Label>
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <p><strong>Estado:</strong>
+                            <asp:Label ID="lblEstado" runat="server" Text="Análisis"></asp:Label>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <p><strong>Tipo:</strong>
+                            <asp:Label ID="lblTipo" runat="server" Text="No conformidad"></asp:Label>
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <p><strong>Modo de Detección:</strong>
+                            <asp:Label ID="lblModoDetect" runat="server" Text="Auditoría Interna"></asp:Label>
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <p><strong>Área afectada:</strong>
+                            <asp:Label ID="lblAreaAfectada" runat="server" Text="Departamento de Finanzas"></asp:Label>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="vertical-align"><strong>Detalle de la No Conformidad:</strong></p>
+                        <asp:TextBox ID="txtDetalle" TextMode="multiline" Width="100%" Rows="5" runat="server"></asp:TextBox> 
+                </div>
+            </div>
+
+
+         </div>
+    </div>
+
+
+
+
+
+
+
+
 </asp:Content>
--- Fuentes/ModulOtec/ModulOtec.Vista/AnalizarNoConformidad.aspx.designer.cs
+++ Fuentes/ModulOtec/ModulOtec.Vista/AnalizarNoConformidad.aspx.designer.cs
@@ -11,5 +11,77 @@ namespace ModulOtec.Vista {
     
     
     public partial class AnalizarNoConformidad {
+        
+        /// <summary>
+        /// Control lblCodSNC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCodSNC;
+        
+        /// <summary>
+        /// Control lblFechaIngreso.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaIngreso;
+        
+        /// <summary>
+        /// Control lblFechaIdentificacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaIdentificacion;
+        
+        /// <summary>
+        /// Control lblEstado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEstado;
+        
+        /// <summary>
+        /// Control lblTipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTipo;
+        
+        /// <summary>
+        /// Control lblModoDetect.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblModoDetect;
+        
+        /// <summary>
+        /// Control lblAreaAfectada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAreaAfectada;
+        
+        /// <summary>
+        /// Control txtDetalle.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDetalle;
     }
 }
