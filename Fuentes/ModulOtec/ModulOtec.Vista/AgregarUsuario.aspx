979c082ec348818521bed8bc363ad94a26c9e5cd
oibz <curarrehueinfo@gmail.com>
2015-12-01 22:40:22 -0300

    Se añade Vista AgregarUsuario.aspx

---
+++ Fuentes/ModulOtec/ModulOtec.Vista/AgregarUsuario.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="AgregarUsuario.aspx.cs" Inherits="ModulOtec.Vista.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <h4>Ingresar Nuevo Usuario</h4>
+    <hr/>
+    <form>
+        <div class="form-group">
+            <label for="txtRut">RUT: </label>
+            <asp:TextBox ID="txtRut" runat="server" MaxLength="8" required="required" Width="100"></asp:TextBox>-<asp:TextBox ID="txtDv" runat="server" MaxLength="1" required="required" Width="20"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="txtNombres">Nombres: </label>
+            <asp:TextBox ID="txtNombres" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="txtApellidoPaterno">Apellido Paterno: </label>
+            <asp:TextBox ID="txtApellidoPaterno" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="txtApellidoMaterno">Apellido Materno: </label>
+            <asp:TextBox ID="txtApellidoMaterno" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="txtDireccion">Dirección: </label>
+            <asp:TextBox ID="txtDireccion" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="ddlComuna">Comuna</label>
+            <asp:DropDownList runat="server" ID="ddlComuna"></asp:DropDownList>
+        </div>
+        <div class="form-group">
+            <label for="txtTelefono">Teléfono: </label>
+            <asp:TextBox ID="txtTelefono" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="txtEmail">Email: </label>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+        </div>
+        <div class="form-group">
+            <label for="ddlRol">Rol de Acceso: </label>
+            <asp:DropDownList runat="server" ID="ddlRol"></asp:DropDownList>
+        </div>
+        <div class="form-group">
+            <label for="txtPassword">Password: </label>
+            <asp:TextBox runat="server" ID="txtPassword" type="password"></asp:TextBox>
+        </div>
+        <div class="form-group">
+        <button type="reset" class="btn btn-danger">Cancelar</button>
+        <button type="submit" class="btn btn-success">Agregar Usuario</button>
+        </div>
+    </form>
+</asp:Content>
---
+++ Fuentes/ModulOtec/ModulOtec.Vista/AgregarUsuario.aspx.designer.cs
@@ -0,0 +1,114 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace ModulOtec.Vista {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control txtRut.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRut;
+        
+        /// <summary>
+        /// Control txtDv.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDv;
+        
+        /// <summary>
+        /// Control txtNombres.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombres;
+        
+        /// <summary>
+        /// Control txtApellidoPaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidoPaterno;
+        
+        /// <summary>
+        /// Control txtApellidoMaterno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellidoMaterno;
+        
+        /// <summary>
+        /// Control txtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDireccion;
+        
+        /// <summary>
+        /// Control ddlComuna.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlComuna;
+        
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+        
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// Control ddlRol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlRol;
+        
+        /// <summary>
+        /// Control txtPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
+    }
+}
